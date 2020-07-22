9e79ac04c46923e513eaea0ac8798bdc6d888c0e
Palesa Mokoena <lee-annmokoena@Palesas-Air.hsd1.co.comcast.net>
2020-07-21 18:48:20 -0600

    defined app routing methods in app.py and imported required modules and object

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+from pusher import pusher
+
+app = Flask(__name__)
+
+#configure puher object
+
+pusher = Pusher(
+app_id = '1042883',
+key    = 'bed9d59bf5e0a729296e',
+secret = '6ff7ed7d1573ca20e013',
+cluster = 'us3',
+ssl     = True )
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route('dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+
+@app.route('/orders', methods=['POST'])
+def order():
+	data = request.form 
+	pusher.trigger(u'message', u'send',
+	{u'name': data['name'],
+	u'message': data['message']})
+	return "message sent"
+
+@app.route('/customer', methods=['POST'])
+def customer():
+	data = request.form 
+	pusher.trigger(u'customer', u'add', {
+		u'name'     : data['name'],
+		u'position' : data['position']
+		u'office'   : data['office'],
+		u'age'      : data['age'],
+		u'salary'   : data['salary'],
+		})
+	return "customer added"
+
+if __name__ == '__main__':
+	app.run(debug=True)
